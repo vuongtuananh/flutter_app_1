402eb74adeb5e6beb14df4b6f060aa8542eee3f3
Anh Vuong Tuan <kuzun9x@gmail.com>
2022-12-21 23:03:51 +0700

    work with Image

--- lib/location_detail.dart
+++ lib/location_detail.dart
@@ -5,7 +5,7 @@ class LocationDetail extends StatelessWidget {
   // const LocationDetail({super.key});
 
   final Location location;
-  LocationDetail(this.location);
+  const LocationDetail(this.location, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,19 +21,26 @@ class LocationDetail extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         textDirection: TextDirection.rtl,
-        children: _renderFacts(location),
+        children: _renderBody(context, location),
       ),
     );
   }
 
-  Widget _section(String title, Color color) {
-    return Container(
-        decoration: BoxDecoration(color: color),
-        child: Text(title),
-        height: 200);
+  // Widget _section(String title, Color color) {
+  //   return Container(
+  //       decoration: BoxDecoration(color: color),
+  //       child: Text(title),
+  //       height: 200);
+  // }
+
+  List<Widget> _renderBody(BuildContext context, Location location) {
+    var result = <Widget>[];
+    result.add(_bannerImage(location.url, 170.0));
+    result.addAll(_renderFacts(context, location));
+    return result;
   }
 
-  List<Widget> _renderFacts(Location location) {
+  List<Widget> _renderFacts(BuildContext context, Location location) {
     var result = <Widget>[];
     for (int i = 0; i < location.facts.length; i++) {
       result.add(_sectionTitle(location.facts[i].title));
@@ -43,10 +50,33 @@ class LocationDetail extends StatelessWidget {
   }
 
   Widget _sectionTitle(String title) {
-    return Text(title);
+    return Container(
+      padding: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 10.0),
+      child: Text(
+        title,
+        textAlign: TextAlign.left,
+        style: const TextStyle(
+          fontSize: 25.0,
+          color: Colors.black,
+        ),
+      ),
+    );
   }
 
   Widget _sectionText(String text) {
-    return Text(text);
+    return Container(
+      padding: EdgeInsets.fromLTRB(25.0, 15.0, 25.0, 15.0),
+      child: Text(text),
+    );
+  }
+
+  Widget _bannerImage(String url, double height) {
+    return Container(
+      constraints: BoxConstraints.tightForFinite(height: height),
+      child: Image.network(
+        url,
+        fit: BoxFit.fitWidth,
+      ),
+    );
   }
 }
--- lib/mocks/mock_location.dart
+++ lib/mocks/mock_location.dart
@@ -7,19 +7,18 @@ class MockLocation extends Location {
   static Location FetchAny() {
     return Location(
         name: 'New York City',
-        url: 'https://goo.gl/Npy2K8',
+        url:
+            'https://www.dogtime.com/assets/uploads/2011/03/puppy-development-1280x720.jpg',
         facts: <LocationFact>[
           LocationFact(
-            title: 'Location Face 1',
-            text: 'This is location fact 1',
+            title: 'Summary',
+            text:
+                'While we could go on about the ethereal glow and seemingly endless heights of this bamboo grove on the outskirts of Kyoto, the sight\'s pleasures extend beyond the visual realm',
           ),
           LocationFact(
-            title: 'Location Face 2',
-            text: 'This is location fact 2',
-          ),
-          LocationFact(
-            title: 'Location Face 3',
-            text: 'This is location fact 3',
+            title: 'How to Get There',
+            text:
+                'Kyoto airport, with several terminals, is located 15 km south of the city and is also known as Kyoto, Kyoto can also be reached by transport kinks from other regional airports',
           ),
         ]);
   }
